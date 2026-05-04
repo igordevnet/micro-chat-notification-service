0a95b2a05f3e432b6f1463eb5126b00031943de7
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-05-03 22:20:43 -0300

    Chore: Ensuring notification flow works, rabbitmq config tweaks

--- src/main/java/com/example/microchatnotificationservice/infrastructure/config/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/example/microchatnotificationservice/infrastructure/config/rabbitmq/RabbitMQConfig.java
@@ -1,20 +1,26 @@
 package com.example.microchatnotificationservice.infrastructure.config.rabbitmq;
 
+import com.example.microchatnotificationservice.controller.dto.NotificationEventDto;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
     public static final String EXCHANGE_NAME = "chat.topic";
 
-    @Value("${rabbitmq.queue.name}")
+    @Value("${spring.rabbitmq.queue.name}")
     private String QUEUE_NAME;
 
     @Bean
@@ -34,7 +40,22 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+
+        typeMapper.setTrustedPackages("*");
+
+        Map<String, Class<?>> idClassMapping = new HashMap<>();
+
+        idClassMapping.put(
+                "com.microservice.microchatmessagingservice.controller.dtos.NotificationEventDto",
+                NotificationEventDto.class
+        );
+
+        typeMapper.setIdClassMapping(idClassMapping);
+        converter.setJavaTypeMapper(typeMapper);
+
+        return converter;
     }
 
     @Bean
